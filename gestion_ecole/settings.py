b82857f3ad7d5f7cc363f26743e4d38edcd08e8f
Rollanda437 <zossourolande93@gmail.com>
2025-11-17 16:05:27 +0100

    FINAL : staticfiles complet + logo + CSS + tout

--- gestion_ecole/settings.py
+++ gestion_ecole/settings.py
@@ -77,3 +77,4 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
