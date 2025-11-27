bdd801bf7b49d731cb109ea09cf38581311c796a
SJCJ Admin <zossourolande93@gmail.com>
2025-11-27 20:01:14 +0100

    AVIS 100%

--- avis/urls.py
+++ avis/urls.py
@@ -3,8 +3,7 @@ from . import views
 
 app_name = 'avis'  # <--- TRÈS IMPORTANT : namespace !
 urlpatterns = [
-    path('', views.index_avis, name='index_avis'), 
-    path('admin-only/', views.liste_avis, name='liste_avis_admin'),  # /avis/admin-only/
+    path('', views.index_avis, name='index_avis'),   # /avis/admin-only/
     path('liste/', views.liste_avis, name='liste_avis'),              # /avis/liste/
     path('<int:avis_id>/', views.detail_avis, name='detail_avis'),    # /avis/1/
     path('<int:avis_id>/ajouter_commentaire/', views.ajouter_commentaire, name='ajouter_commentaire'),
