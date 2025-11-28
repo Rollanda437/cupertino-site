664216179feafb700ee92e844e5eda2fbfacb496
SJCJ Admin <zossourolande93@gmail.com>
2025-11-28 19:16:52 +0100

    Config finale infaillible – base dans /tmp + migrations auto

--- gestion_ecole/asgi.py
+++ gestion_ecole/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cupertino.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gestion_ecole.settings')
 
 application = get_asgi_application()
