18e55d5b4144a98adfa81451445931dfe7748cfc
Rollanda437 <zossourolande93@gmail.com>
2025-11-30 19:01:51 +0100

    modifier avec sucess des eleves1452103

--- api/index.py
+++ api/index.py
@@ -1,11 +1,22 @@
-
-    # api/index.py → CONFIGURATION OFFICIELLE VERCEL + DJANGO 2025
 import os
 from django.core.wsgi import get_wsgi_application
 
-# Configuration Django
+# Ajout des lignes de migration
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gestion_ecole.settings')
 application = get_wsgi_application()
+
+# --- BLOC D'EXÉCUTION DE MIGRATION ---
+from django.core.management import call_command
+from django.db.utils import OperationalError
+
+try:
+    print("Tentative d'exécution des migrations sur le démarrage...")
+    call_command('migrate', interactive=False)
+    print("Migrations terminées avec succès.")
+except OperationalError as e:
+    # Ceci peut arriver si la base de données n'est pas encore accessible
+    print(f"Erreur lors de l'exécution des migrations : {e}")
+# ------------------------------------
 
 # Vercel exige exactement cette variable
 app = application
